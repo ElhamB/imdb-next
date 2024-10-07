da8130ba04dc39f18662ad930c3cf1c051689ccc
elham <bagheri_e@cyberno.ir>
2024-10-07 11:20:11 +0330

    adds loading and set image config in nextjs config

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -21,14 +21,14 @@ export default function Card({ result }: CardProps) {
     return (
         <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
             <Link href={`/movie/${id}`}>
-                {/* <Image
+                <Image
                     src={`https://image.tmdb.org/t/p/original/${backdrop_path || poster_path
                         }`}
                     width={500}
                     height={300}
                     alt="image"
                     className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
-                ></Image> */}
+                ></Image>
                 <div className='p-2'>
                     <p className='line-clamp-2 text-md'>{overview}</p>
                     <h2 className='text-lg font-bold truncate'>
